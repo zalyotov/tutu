d03a0b8b30445be4b608ce610d989ef5368c849a
Evgeny Zaletov <zalyotov@mail.ru>
2017-05-15 18:05:58 +0300

    Убрана валидация связанных объектов

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,8 +1,8 @@
 class Train < ApplicationRecord
   # поезд связан с одной станцией
-  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
   # поезд принадлежит маршруту
-  belongs_to :route
+  belongs_to :route, optional: true
   # поезд содержит информацию о билетах
   has_many :tickets
 
