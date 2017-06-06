c6f83ca627595909e64412a4112f454af2c5add0
Evgeny Zaletov <zalyotov@mail.ru>
2017-06-06 15:57:42 +0300

    Добавлен порядковый номер станции, список станций в маршруте выводится в соответствии с сортировкой

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,5 +3,7 @@ class Route < ApplicationRecord
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
 
+  #scope :sort, -> { order(:sort) }
+
   validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518094832) do
+ActiveRecord::Schema.define(version: 20170606101945) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170518094832) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "sort"
   end
 
   create_table "routes", force: :cascade do |t|
