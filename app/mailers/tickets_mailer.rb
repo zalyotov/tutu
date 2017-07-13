3c4a265bf5e9ba28ee0b11365a0ee24959e6f133
Evgeny Zaletov <zalyotov@mail.ru>
2017-07-13 21:38:02 +0300

    Реализована отправка писем при покупке и отмене покупки билета

---
+++ app/mailers/tickets_mailer.rb
@@ -0,0 +1,12 @@
+class TicketsMailer < ApplicationMailer
+  def buy_ticket(user, ticket)
+    @user = user
+    @ticket = ticket
+    mail(to: user.email, subject: 'Вы купили билет')
+  end
+  def destroy_ticket(user, ticket)
+    @user = user
+    @ticket = ticket
+    mail(to: user.email, subject: 'Вы отменили покупку билета')
+  end
+end
