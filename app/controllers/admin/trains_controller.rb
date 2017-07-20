6d724390f670ea404374706eee6b3444400c268e
Evgeny Zaletov <zalyotov@mail.ru>
2017-07-20 13:37:29 +0300

    Добавлено инлайн редактирование поездов

--- app/controllers/admin/trains_controller.rb
+++ app/controllers/admin/trains_controller.rb
@@ -36,7 +36,7 @@ class Admin::TrainsController < Admin::BaseController
   def update
     respond_to do |format|
       if @train.update(train_params)
-        format.html { redirect_to [:admin, @train], notice: 'Train was successfully updated.' }
+        format.html { redirect_to admin_trains_path, notice: 'Train was successfully updated.' }
       else
         format.html { render :edit }
       end
